6057f6d549ef1a9e006603a16b779599a5522dbf
Elie Zananiri <prisonerjohn@gmail.com>
2014-06-02 22:29:40 -0400

    Updated FRENCH keyboard layouts. Looks like this extended-ASCII stuff only works on Windows.

--- src/ofxSoftKeyboard.cpp
+++ src/ofxSoftKeyboard.cpp
@@ -60,28 +60,28 @@ void ofxSoftKeyboard::setLayout(ofxSoftKeyboardLayout _layout) {
 			break;
 
 		case OFXSK_LAYOUT_KEYBOARD_FULL_FRENCH:
-			addKey('~'); addKey('1'); addKey('2'); addKey('3'); addKey('4'); addKey('5'); addKey('6'); addKey('7'); addKey('8'); addKey('9'); addKey('0'); addKey('-'); addKey('+'); addKey(OFXSK_KEY_DELETE); newRow();
-			addKey('à').padLeft(52); addKey('â'); addKey('é'); addKey('è'); addKey('ê'); addKey('ë'); addKey('ï'); addKey('î'); addKey('ô'); addKey('ù'); addKey('û'); addKey('ü'); addKey('ç'); newRow();
-			addKey(OFXSK_KEY_TAB); addKey('q'); addKey('w'); addKey('e'); addKey('r'); addKey('t'); addKey('y'); addKey('u'); addKey('i'); addKey('o'); addKey('p'); addKey('['); addKey(']'); addKey('|'); newRow();
-			addKey(OFXSK_KEY_CAPS); ; addKey('a'); addKey('s'); addKey('d'); addKey('f'); addKey('g'); addKey('h'); addKey('j'); addKey('k'); addKey('l'); addKey(';'); addKey('\''); addKey(OFXSK_KEY_RETURN); newRow();
-			addKey(OFXSK_KEY_SHIFT); addKey('z'); addKey('x'); addKey('c'); addKey('v'); addKey('b'); addKey('n'); addKey('m'); addKey(','); addKey('.'); addKey('/'); addKey(OFXSK_KEY_SHIFT); newRow();
+			addKey('`', '~'); addKey('1', '!'); addKey('2', '@'); addKey('3', '#'); addKey('4', '$'); addKey('5', '%'); addKey('6', '^'); addKey('7', '&'); addKey('8', '*'); addKey('9', '('); addKey('0', ')'); addKey('-', '_'); addKey('=', '+'); addKey(OFXSK_KEY_DELETE); newRow();
+			addKey('à', 'À').padLeft(52); addKey('â', 'Â'); addKey('ç', 'Ç'); addKey('é', 'É'); addKey('è', 'È'); addKey('ê', 'Ê'); addKey('ë', 'Ë'); addKey('ï', 'Ï'); addKey('î', 'Î'); addKey('ô', 'Ô'); addKey('ù', 'Ù'); addKey('û', 'Û'); addKey('ü', 'Ü'); newRow();
+			addKey(OFXSK_KEY_TAB); addKey('q'); addKey('w'); addKey('e'); addKey('r'); addKey('t'); addKey('y'); addKey('u'); addKey('i'); addKey('o'); addKey('p'); addKey('[', '{'); addKey(']', '}'); addKey('\\', '|'); newRow();
+			addKey(OFXSK_KEY_CAPS); ; addKey('a'); addKey('s'); addKey('d'); addKey('f'); addKey('g'); addKey('h'); addKey('j'); addKey('k'); addKey('l'); addKey(';', ':'); addKey('\'', '"'); addKey(OFXSK_KEY_RETURN); newRow();
+			addKey(OFXSK_KEY_SHIFT); addKey('z'); addKey('x'); addKey('c'); addKey('v'); addKey('b'); addKey('n'); addKey('m'); addKey(',', '<'); addKey('.', '>'); addKey('/', '?'); addKey(OFXSK_KEY_SHIFT); newRow();
 			addKey(' ').padLeft(254).setSize(300, 40);
 			break;
 
 		case OFXSK_LAYOUT_KEYBOARD_FORM:
-			addKey('@'); addKey('1'); addKey('2'); addKey('3'); addKey('4'); addKey('5'); addKey('6'); addKey('7'); addKey('8'); addKey('9'); addKey('0'); addKey('-'); addKey('+'); addKey(OFXSK_KEY_DELETE); newRow();
+			addKey('1').padLeft(52); addKey('2'); addKey('3'); addKey('4'); addKey('5'); addKey('6'); addKey('7'); addKey('8'); addKey('9'); addKey('0'); addKey('-'); addKey('+'); addKey(OFXSK_KEY_DELETE); newRow();
 			addKey(OFXSK_KEY_TAB); addKey('q'); addKey('w'); addKey('e'); addKey('r'); addKey('t'); addKey('y'); addKey('u'); addKey('i'); addKey('o'); addKey('p'); addKey('_'); addKey('*'); newRow();
 			addKey(OFXSK_KEY_CAPS); ; addKey('a'); addKey('s'); addKey('d'); addKey('f'); addKey('g'); addKey('h'); addKey('j'); addKey('k'); addKey('l'); addKey(';'); addKey('\''); addKey(OFXSK_KEY_RETURN); newRow();
-			addKey(OFXSK_KEY_SHIFT); addKey('z'); addKey('x'); addKey('c'); addKey('v'); addKey('b'); addKey('n'); addKey('m'); addKey(','); addKey('.'); addKey('/'); addKey(OFXSK_KEY_SHIFT); newRow();
+			addKey(OFXSK_KEY_SHIFT); addKey('z'); addKey('x'); addKey('c'); addKey('v'); addKey('b'); addKey('n'); addKey('m'); addKey(','); addKey('.'); addKey('@'); addKey(OFXSK_KEY_SHIFT); newRow();
 			addKey(' ').padLeft(254).setSize(300, 40);
 			break;
 
 		case OFXSK_LAYOUT_KEYBOARD_FORM_FRENCH:
-			addKey('@'); addKey('1'); addKey('2'); addKey('3'); addKey('4'); addKey('5'); addKey('6'); addKey('7'); addKey('8'); addKey('9'); addKey('0'); addKey('-'); addKey('+'); addKey(OFXSK_KEY_DELETE); newRow();
-			//addKey('ï¿½').padLeft(52); addKey('ï¿½'); addKey('ï¿½'); addKey('ï¿½'); addKey('ï¿½'); addKey('ï¿½'); addKey('ï¿½'); addKey('ï¿½'); addKey('ï¿½'); addKey('ï¿½'); addKey('ï¿½'); addKey('ï¿½'); addKey('ï¿½'); newRow();
+			addKey('1').padLeft(52); addKey('2'); addKey('3'); addKey('4'); addKey('5'); addKey('6'); addKey('7'); addKey('8'); addKey('9'); addKey('0'); addKey('-'); addKey('+'); addKey(OFXSK_KEY_DELETE); newRow();
+			addKey('à', 'À').padLeft(52); addKey('â', 'Â'); addKey('ç', 'Ç'); addKey('é', 'É'); addKey('è', 'È'); addKey('ê', 'Ê'); addKey('ë', 'Ë'); addKey('ï', 'Ï'); addKey('î', 'Î'); addKey('ô', 'Ô'); addKey('ù', 'Ù'); addKey('û', 'Û'); addKey('ü', 'Ü'); newRow();
 			addKey(OFXSK_KEY_TAB); addKey('q'); addKey('w'); addKey('e'); addKey('r'); addKey('t'); addKey('y'); addKey('u'); addKey('i'); addKey('o'); addKey('p'); addKey('_'); addKey('*'); newRow();
 			addKey(OFXSK_KEY_CAPS); ; addKey('a'); addKey('s'); addKey('d'); addKey('f'); addKey('g'); addKey('h'); addKey('j'); addKey('k'); addKey('l'); addKey(';'); addKey('\''); addKey(OFXSK_KEY_RETURN); newRow();
-			addKey(OFXSK_KEY_SHIFT); addKey('z'); addKey('x'); addKey('c'); addKey('v'); addKey('b'); addKey('n'); addKey('m'); addKey(','); addKey('.'); addKey('/'); addKey(OFXSK_KEY_SHIFT); newRow();
+			addKey(OFXSK_KEY_SHIFT); addKey('z'); addKey('x'); addKey('c'); addKey('v'); addKey('b'); addKey('n'); addKey('m'); addKey(','); addKey('.'); addKey('@'); addKey(OFXSK_KEY_SHIFT); newRow();
 			addKey(' ').padLeft(254).setSize(300, 40);
 			break;
 	}
